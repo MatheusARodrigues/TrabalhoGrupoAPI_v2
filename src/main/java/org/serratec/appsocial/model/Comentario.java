0815e2239e96ad963ed2db49af416e7994e130a5
MatheusARodrigues <matheusadrianor7@gmail.com>
2024-05-24 19:02:33 -0300

    Fix: fazendo correção na classe comentário.

--- src/main/java/org/serratec/appsocial/model/Comentario.java
+++ src/main/java/org/serratec/appsocial/model/Comentario.java
@@ -2,6 +2,8 @@ package org.serratec.appsocial.model;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +19,7 @@ public class Comentario {
 	private String conteudo;
 	private LocalDate dataCriacao;
 
+	@JsonIgnore
 	@ManyToOne
 	private Postagem postagem;
 
